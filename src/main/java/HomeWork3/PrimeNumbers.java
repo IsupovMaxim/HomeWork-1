332503ef13fe422237513f545582982bc4dd3e81
IsupovMaxim <isupovmaxim@gmail.com>
2023-11-04 21:26:41 +0400

    Домашнее задание 3

--- src/main/java/HomeWork3/PrimeNumbers.java
+++ src/main/java/HomeWork3/PrimeNumbers.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class PrimeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число, чтобы узнать каким оно является - состовным или простым.");
+        System.out.println("Введите число, чтобы узнать каким оно является - составным или простым.");
         int x = scanner.nextInt();
         for (int i = 2; i*i<= x; i++){
             if(x % i == 0){
