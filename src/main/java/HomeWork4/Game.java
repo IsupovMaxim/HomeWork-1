f0ce1d160a41eb9a776f0aedfdae93f5d0befd37
IsupovMaxim <isupovmaxim@gmail.com>
2023-11-09 18:05:10 +0400

    Домашнее задание 4

--- src/main/java/HomeWork4/Game.java
+++ src/main/java/HomeWork4/Game.java
@@ -16,10 +16,40 @@ package HomeWork4;
 //        Вычеркивали 4 раза, значит результирующий счет = 4
 //        Примечание: не всегда нужно всю логику хранить в одном методе, можно части кода выносить в отдельные, чтобы потом их переиспользовать
 
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Game {
-//    int[] myArray;
-//    myArray  = new int[10];
+    public static void main(String[] args) {
 
+        ArrayList<Integer> ArrayList = new ArrayList<>(Arrays.asList(6, 8, 0, 5, 5, 1, 8, 4, 7, 2));
+
+        System.out.println("Наш массив: " + ArrayList);
+
+        int result = removeArrayNumbers(ArrayList);
+
+        System.out.println("Результирующий счёт = " + result);
     }
+    private static int removeArrayNumbers(ArrayList<Integer> ArrayList) {
+        int counter = 0;
+        int i = 0;
 
+        while (i < ArrayList.size() - 1) {
+            int value = ArrayList.get(i);
+            int value2 = ArrayList.get(i + 1);
+            int sum = value + value2;
+
+            if (sum == 9 || value == value2) {
+                System.out.println("Сумма чисел " + value + " и " + value2 + " равна 9 или числа равны между собой - удаляются");
+                ArrayList.remove(i + 1);
+                ArrayList.remove(i);
+                counter++;
+                i -= 2;
+                if (i < 0) i = 0;
+            } else {
+                i++;
+            }
+        }
+        return counter;
+    }
+}
